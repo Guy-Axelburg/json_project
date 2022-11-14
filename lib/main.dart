787490f473e5b991fd35844a7e94c884cce9735b
Guy-Axelburg <parkerkouns@live.com>
2022-11-14 15:36:51 -0700

    fixed commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,15 @@ flutter:
 http: ^0.13.5
 */
 
-
-
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
-  runApp(const MaterialApp(
-    home: MyApp(),
-  ));
+  runApp(MaterialApp(home: const MyApp(), routes: <String, WidgetBuilder>{
+    "/HomePage": (BuildContext context) => const MyApp(),
+    "/downloadedPage": (BuildContext context) => const downloadedJson(),
+    "/APIPage": (BuildContext context) => const ApiJson()
+  }));
 }
 
 class MyApp extends StatefulWidget {
@@ -36,174 +36,145 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 1;
 
-
-  int _selectedIndex = 0;
-  static const TextStyle myStyle = TextStyle(fontSize: 36, fontWeight: FontWeight.bold);
-
-  void _onItemTapped(int index)
-  {
-  setState(() {
-  _selectedIndex = index;
-  });
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.of(context).pushNamed(pages[index]);
+    });
   }
 
-  static List<Widget> _widgetOptions = [
-  Text('Index 0: Home Page', style: myStyle),
-  Text('Index 1: API Json', style: myStyle),
-  Text('Index 2: Downloaded Json', style: myStyle),
-  myWidget
-
-  ];
-
-
-  static Widget myWidget = Scrollbar(
-  thickness: 10,
-  thumbVisibility: true,
-  child: ListView.builder(
-  primary: true,
-  itemCount: 50,
-  itemBuilder: (BuildContext context, int index){
-
-  return Container(
-  height: 50,
-  color: index.isEven?
-  Colors.black26:
-  Colors.grey[200],
-  child: Padding(
-  padding: const EdgeInsets.all(10),
-  child: Text('City: '+ cities[index]),
-
-  ),
-
-
-  );
-
-  }
-  ),
-
-  );
-
+  List pages = ["/downloadedPage", "/HomePage", "/APIPage"];
 
   @override
-  Widget build(BuildContext context)
-  {
-  return Scaffold(
-  appBar: AppBar(title: const Text("Json App")),
-  body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
-  bottomNavigationBar: BottomNavigationBar(
-  items: const[
-  BottomNavigationBarItem(
-  icon: Icon(Icons.wifi),
-  label: 'Online',
-  backgroundColor: Colors.blueAccent),
-  BottomNavigationBarItem(
-  icon: Icon(Icons.home),
-  label: 'Home',
-  backgroundColor: Colors.yellowAccent),
-  BottomNavigationBarItem(
-  icon: Icon(Icons.download_for_offline_outlined),
-  label: 'Downloaded',
-  backgroundColor: Colors.blueGrey),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Json App")),
+      body: Container(
+        alignment: Alignment.center,
+        child: Text("Welcome to my JSON Project App! Click the left"
+            " button to go to the Ikea downloaded JSON page."
+            " Tap the bottom right button to see the API JSON of Magic Spells!")
 
 
-
-  ],
-  currentIndex: _selectedIndex,
-  selectedItemColor: Colors.amber[800],
-  onTap: _onItemTapped,
-
-  ),
-  );
-
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.download_for_offline_outlined),
+              label: 'Downloaded',
+              backgroundColor: Colors.blueGrey),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.yellowAccent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wifi),
+              label: 'Online API',
+              backgroundColor: Colors.blueAccent),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
-
-
 }
 
-
-class apiJson extends StatefulWidget {
-  const apiJson({Key? key}) : super(key: key);
+class ApiJson extends StatefulWidget {
+  const ApiJson({Key? key}) : super(key: key);
 
   @override
-  _apiJson createState() => _apiJson();
+  _ApiJson createState() => _ApiJson();
+}
 
-    }
-
-    class _apiJson extends State <apiJson>{
-
+class _ApiJson extends State<ApiJson> {
   static var data;
 
   static Future getData() async {
-
-    var url = Uri.parse("https://wizard-world-api.herokuapp.com/Spells");
+    var url = Uri.parse("https://flutter.locusnoesis.com/spellsinfo.php");
 
     //http.Response response = await http.get(url);
     http.Response response = await http.post(url);
     data = await jsonDecode(response.body);
 
     return data;
-
   }
-
 
   Widget myWidget = FutureBuilder(
       future: getData(),
-      builder: (BuildContext context, AsyncSnapshot snapshot)
-      {
-        if(snapshot.hasData)
-        {
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
           return Scrollbar(
               interactive: true,
               thickness: 10,
               thumbVisibility: true,
-              child:
-              ListView.builder(
+              child: ListView.builder(
                   itemCount: data.length,
-                  itemBuilder: (BuildContext context, int index){
-                    return
-                      Card(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                ListTile(
-                                    title: Text(data[index]['year'].toString() +
-                                        " " + data[index]['make']),
-                                    subtitle: Text(data[index]['body_styles'])
-                                )
-                              ]
-                          )
-                      );
-                  }
-              )
-          );
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                          ListTile(
+                              title: Text(data[index]['spellname'] +
+                                  ": " +
+                                  data[index]['incantation']),
+                              subtitle: Text(data[index]['effect']))
+                        ]));
+                  }));
         }
         return const Text("Waiting For Data");
-      }
-  );
+      });
+
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.of(context).pushNamed(pages[index]);
+    });
+  }
+
+  List pages = ["/downloadedPage", "/HomePage", "/APIPage"];
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Autos Json API"),
+        appBar: AppBar(
+            title: const Text("JSON from API"),
             backgroundColor: Colors.indigo),
-        body: Column(
-            children: [
-              Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.all(20),
-                  child: const Text("Cars For Sale")
-              ),
-              Expanded(child:  myWidget)
-            ]
-        )
-    );}
+        body: Column(children: [
+          Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(20),
+              child: const Text("Spells")),
+          Expanded(child: myWidget)
+        ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.download_for_offline_outlined),
+              label: 'Downloaded',
+              backgroundColor: Colors.blueGrey),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.yellowAccent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wifi),
+              label: 'Online API',
+              backgroundColor: Colors.blueAccent),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),);
+  }
 }
-
-
-
 
 class downloadedJson extends StatefulWidget {
   const downloadedJson({Key? key}) : super(key: key);
@@ -213,63 +184,84 @@ class downloadedJson extends StatefulWidget {
 }
 
 class _downloadedJson extends State<downloadedJson> {
-  List _Movies = [];
+  List _ikea = [];
 
   //fetch content from json file
   Future<void> readJson() async {
-    final String response =
-    await rootBundle.loadString("assets/movies_array.json");
+    final String response = await rootBundle.loadString("assets/ikea.json");
     final data = await json.decode(response);
 
     setState(() {
-      _Movies = data;
+      _ikea = data;
     });
   }
+
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.of(context).pushNamed(pages[index]);
+    });
+  }
+
+  List pages = ["/downloadedPage", "/HomePage", "/APIPage"];
+
+  var styler = TextStyle(color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar:
-        AppBar(centerTitle: true, title: const Text("Read Json - Movies")),
+            AppBar(centerTitle: true, title: const Text("Read Downloaded Json - Ikea Catalogue")),
         body: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(children: [
             ElevatedButton(child: const Text('Load Data'), onPressed: readJson),
-            _Movies.isNotEmpty
+            _ikea.isNotEmpty
                 ? Expanded(
-                child: ListView.builder(
-                    itemCount: _Movies.length,
-                    itemBuilder: (context, index) {
-                      //example with card
-                      return /* Card(
-                              margin: const EdgeInsets.all(10),
-                              child: ListTile(
-                                  leading: Text(_Movies[index][0].toString()),
-                                  title: Text(_Movies[index][1]),
-                                  subtitle: Text(_Movies[index][6].toString()))); */
-
-
-                        (
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Rank ${_Movies[index][0]} "),
-                                  Text(_Movies[index][1].toString()),
-                                  Text(_Movies[index][2].toString() ),
-                                  Text("Rating: ${_Movies[index][3]}"),
-                                  Text("Year: ${_Movies[index][4]}"),
-                                  Text("Genre: ${_Movies[index][6]}"),
-                                  const Divider(height: 10, thickness: 3, color: Colors.indigo)
-
-                                ],
-                              ),
-                            )
-
-                        );
-                    }))
+                    child: ListView.builder(
+                        itemCount: _ikea.length,
+                        itemBuilder: (context, index) {
+                          //example with card
+                          return (Container(color: Colors.lightBlue,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Item ID#" + _ikea[index][1].toString(), style: styler),
+                                Text("Item Name: ${_ikea[index][2]}", style: styler),
+                                Text("Category: ${_ikea[index][3]}", style: styler),
+                                Text("Price: ${_ikea[index][4]}", style: styler),
+                                const Divider(
+                                    height: 10,
+                                    thickness: 3,
+                                    color: Colors.yellow)
+                              ],
+                            ),
+                          ));
+                        }))
                 : Container()
           ]),
-        ));
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.download_for_offline_outlined),
+              label: 'Downloaded',
+              backgroundColor: Colors.blueGrey),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.yellowAccent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wifi),
+              label: 'Online API',
+              backgroundColor: Colors.blueAccent),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),);
   }
 }
